0bc0a525f3e723d99a2782f5de89d1ef801e6fd8
Akshaya-875 <menakaharivarsha@gmail.com>
2025-05-23 15:27:37 +0530

    Initial commit

---
+++ Hospital-Mangement-React-main/Frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Hospital Management System. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
